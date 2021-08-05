6f4126303dc8ad0c43f6af65ece6aa4b4bd2e420
tayngo97 <anhtayd27111997@gmail.com>
2021-08-05 17:05:47 +0700

    push len de doi may tinh lam tiep

---
+++ module1/Bien_kieudulieu_toantu/Bai tap/sudungtoantu.js
@@ -0,0 +1,14 @@
+let inputHeight;
+let inputWidth ;
+
+inputHeight = prompt("Nhập vào chiều rộng");
+inputWidth = prompt("Nhạp vào chiều cao");
+
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+
+let area = width*height
+document.write("dien tich hinh chu nhat :"+ area)
+
