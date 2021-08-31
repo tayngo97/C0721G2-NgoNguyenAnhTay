00d045d3b71e5e2f4e12c7ad6666d100d7dbec58
tayngo97 <anhtayd27111997@gmail.com>
2021-08-31 14:48:04 +0700

    update

---
+++ module_2/src/Introduction/Systemtime/Systemtime.java
@@ -0,0 +1,10 @@
+package Introduction.Systemtime;
+
+import java.util.Date;
+
+public class Systemtime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is; " + now);
+    }
+}
--- module_2/src/hello_world/classtext.java
+++
@@ -1,7 +0,0 @@
-package hello_world;
-
-public class classtext {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
