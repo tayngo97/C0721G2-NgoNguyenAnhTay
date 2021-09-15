d0bd78e18c42c7d8b4150df7f94745696dec285e
tayngo97 <anhtayd27111997@gmail.com>
2021-09-15 07:41:30 +0700

    update bai tap

--- module_2/src/bai10_dsa_danhsach/baitap/trienkhai_linkedlist/MyLinkedList.java
+++ module_2/src/bai10_dsa_danhsach/baitap/trienkhai_linkedlist/MyLinkedList.java
@@ -4,15 +4,17 @@ public class MyLinkedList<E> {
     private Node head;
     private int numNodes = 0;
 
-    public MyLinkedList() {
-
+    public MyLinkedList(){
     }
 
     private class Node {
         private Node next;
         private Object data;
 
-        public Node(Object data) {
+        public Node(E e) {
+        }
+
+        public void Node(Object data) {
             this.data = data;
         }
 
@@ -21,120 +23,104 @@ public class MyLinkedList<E> {
         }
     }
 
-    public int size() {
+    public int size(){
         return numNodes;
     }
 
-    public void add(int index, E element) {
-        Node temp = head;
-        Node holder;
-
-        for (int i = 0; i < index - 1 && temp.next != null; i++) {
-            temp = temp.next;
-        }
-        holder = temp.next;
-        temp.next = new Node(element);
-        temp.next.next = holder;
-        numNodes++;
-
-    }
-
-    public void addFirst(E element) {
-        Node temp = head;
-        head = new Node(element);
-        head.next = temp;
-        numNodes++;
-    }
-
-    public void addLast(E element) {
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = new Node(element);
-        numNodes++;
-    }
-
-    public Object get(int index) {
-        Node temp = head;
-        for (int i = 0; i < index; i++) {
+    public Object get(int index){
+        Node temp =head;
+        for(int i = 0; i < index; i++){
             temp = temp.next;
         }
         return temp.data;
     }
 
-    public boolean contains(E o) {
+    public boolean contains(E element) {
         Node temp = head;
-        for (int i = 0; i < numNodes; i++) {
-            if (temp.getData().equals(o)) {
+
+        while (temp.next != null) {
+            if(temp.data.equals(element)){
                 return true;
             }
             temp = temp.next;
         }
+        if ( temp.data.equals(element)){
+            return true;
+        }
         return false;
     }
 
-    public int indexOf(E o) {
+    public int indexOf(E element){
         Node temp = head;
-        for (int i = 0; i < numNodes; i++) {
-            if (temp.getData().equals(o)) {
+        for (int i = 0; i < numNodes;i++){
+            if(temp.getData().equals(element))
                 return i;
-            }
             temp = temp.next;
         }
-        return -1;
+        return  -1;
     }
 
-    public E remove(int index) {
-        if (index < 0 || index > numNodes) {
+    public void addLast (E e) {
+        Node temp = head;
+        while (temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = new Node(e);
+        numNodes++;
+    }
+
+    public void addFirst (E e) {
+        Node temp = head;
+        head = new Node(e);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public void add (int index, E data) {
+        Node temp = head;
+        Node holder;
+        for(int i = 0; i < (index - 1) && temp.next != null;i++){
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public E remove ( int index){
+        if(index < 0 || index > numNodes){
             throw new IndexOutOfBoundsException();
         }
         Node temp = head;
         Object data;
-        if (index == 0) {
-            data = temp.getData();
+        if(index == 0){
+            data = temp.data;
             head = head.next;
-        } else {
-            for (int i = 0; i < index - 1 && temp.next != null; i++) {
+        }else {
+            for(int i = 0; i < index - 1 && temp.next != null; i++){
                 temp = temp.next;
             }
-            data = temp.next.getData();
+            data = temp.next.data;
             temp.next = temp.next.next;
         }
         numNodes--;
         return (E) data;
     }
 
-    public boolean remove(E o) {
-        if (head.getData().equals(o)) {
-            remove(o);
-            return true;
-        } else {
-            Node temp = head;
-            while (temp.next != null) {
-                if (temp.getData().equals(o)) {
-                    temp.next = temp.next.next;
-                    numNodes--;
-                    return true;
-                }
-                temp = temp.next;
-            }
-        }
-        return false;
-    }
-
-    public E clone() {
-        if (numNodes == 0) {
+    public MyLinkedList<E> clone(){
+        if(numNodes == 0) {
             throw new NullPointerException();
         }
-        MyLinkedList<Object> temp = new MyLinkedList<>();
+        MyLinkedList<E> temp = new MyLinkedList<>();
         Node tempNode = head;
-        temp.addFirst(tempNode.getData());
+        temp.addFirst((E) tempNode.data);
         tempNode = tempNode.next;
-        while (tempNode != null) {
-            temp.addLast(tempNode.getData());
+        while (tempNode != null){
+            temp.addFirst((E) tempNode.data);
             tempNode = tempNode.next;
         }
-        return (E) temp;
+        return temp;
     }
+
 }
